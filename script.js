017c0a6a93621a41cdc382fa907573d7180dab46
arpitajangir1111 <arpita8857@gmail.com>
2023-10-09 23:12:35 -0700

    first

---
+++ script.js
@@ -0,0 +1,62 @@
+// let isLoggedIn =1;
+
+// if(isLoggedIn ==1){
+// document.write("Login")
+// }
+
+// else{
+//     document.write("LogOut")
+// }
+
+
+// TERNARY OPERATOR
+
+// let option = isLoggedIn==1 ? "login" : "logOut"
+// document.write(option)
+
+
+
+// let user;
+// user ="arpita"
+// alert(user ?? "bitu")
+
+// in this if user is defined then it will print arpita if user in undefined then it will print thevalue written after the ?? 
+
+
+// let input;
+// input = "" ;
+
+// if(){
+
+
+// }
+// else if(){
+    
+// }
+// else if(){
+    
+// }
+// else if(){
+    
+// }
+// else if(){
+    
+// }
+// else if(){
+    
+// }
+// else (){
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
